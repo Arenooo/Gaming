53259d66fa86faf103c1ac46b0675d2c1117403f
funkvay-star <pasha1928374655@gmail.com>
2022-12-24 17:51:00 +0400

    Resolved review issues
    Removed default comments,
    Added access modifiers,
    Fixed typo,
    Renamed PlayerMovement to PlayerController

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -9,19 +9,17 @@ public class CameraController : MonoBehaviour
 
 	private float _lastXPosition;
 
-	// Start is called before the first frame update
-	void Start()
+	private void Start()
 	{
 		_lastXPosition = transform.position.x;
 	}
 
-	// Update is called once per frame
-	void Update()
+	private void Update()
 	{
-		FollowTarger();
+		FollowTarget();
 	}
 
-	void FollowTarger()
+	void FollowTarget()
 	{
 		transform.position = new Vector3(_target.position.x, transform.position.y, _target.position.z);
 	}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,21 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private float _runSpeed;
 	
 	private Rigidbody _playersRigidBody;
 
-	// Start is called before the first frame update
-	void Start()
+	private void Start()
 	{
 		_playersRigidBody = GetComponent<Rigidbody>();
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
+	private void Update()
+	{	
 		MovePlayer();
 	}
 
